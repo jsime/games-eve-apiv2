bbe3130fe39cbdff2fb6e24cea1a9e49db3b983f
Jon Sime <jonsime@gmail.com>
2013-06-17 15:53:15 -0400

    updated attribute definitions for ::Request

--- lib/Games/EVE/APIv2/Request.pm
+++ lib/Games/EVE/APIv2/Request.pm
@@ -1,20 +1,29 @@
 package Games::EVE::APIv2::Request;
 
+use LWP::UserAgent;
+use XML::LibXML;
+
 use Moose;
 
 has 'api' => (
-    is  => 'rw',
-    isa => 'Str',
+    is        => 'rw',
+    isa       => 'Str',
+    clearer   => 'clear_api',
+    predicate => 'has_api',
 );
 
 has 'key_id' => (
-    is  => 'rw',
-    isa => 'Int',
+    is        => 'rw',
+    isa       => 'Int',
+    clearer   => 'clear_key_id',
+    predicate => 'has_key_id',
 );
 
 has 'v_code' => (
-    is  => 'rw',
-    isa => 'Str',
+    is        => 'rw',
+    isa       => 'Str',
+    clearer   => 'clear_v_code',
+    predicate => 'has_v_code',
 );
 
 sub get {
