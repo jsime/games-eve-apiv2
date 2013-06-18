452c419728dd7055c8b59e3c1b5d5c34368dfa02
Jon Sime <jonsime@gmail.com>
2013-06-17 20:02:39 -0400

    synopsis and a couple base attributes (the rest to be filled in later)

--- lib/Games/EVE/APIv2/Character.pm
+++ lib/Games/EVE/APIv2/Character.pm
@@ -1,5 +1,17 @@
 package Games::EVE::APIv2::Character;
 
+=head1 NAME
+
+Games::EVE::APIv2::Character
+
+=head1 SYNOPSIS
+
+Subclass providing access to individual pilot details. Calls to some of the
+methods in the base C<Games::EVE::APIv2> class are modified in the context
+of a Character object.
+
+=cut
+
 use strict;
 use warnings FATAL => 'all';
 use namespace::autoclean;
@@ -7,6 +19,27 @@ use namespace::autoclean;
 use Moose;
 
 extends 'Games::EVE::APIv2';
+
+has 'key_id' => (
+    is  => 'ro',
+    isa => 'Int',
+);
+
+has 'v_code' => (
+    is  => 'ro',
+    isa => 'Str',
+);
+
+has 'character_id' => (
+    is        => 'ro',
+    isa       => 'Int',
+    predicate => 'has_character_id',
+);
+
+has 'name' => (
+    is  => 'ro',
+    isa => 'Str',
+);
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
