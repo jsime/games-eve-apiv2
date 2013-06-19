47ec5845c36cdcb6362c9a4307af2bf1fc264f68
Jon Sime <jonsime@gmail.com>
2013-06-19 01:50:30 -0400

    stub for corporation API calls

---
+++ lib/Games/EVE/APIv2/Corporation.pm
@@ -0,0 +1,43 @@
+package Games::EVE::APIv2::Corporation;
+
+=head1 NAME
+
+Games::EVE::APIv2::Corporation
+
+=head1 SYNOPSIS
+
+Subclass providing access to corporation information.
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use namespace::autoclean;
+
+extends 'Games::EVE::APIv2::Base';
+
+has 'corporation_id' => (
+    is        => 'ro',
+    isa       => 'Num',
+    predicate => 'has_corporation_id',
+);
+
+has 'name' => (
+    is     => 'rw',
+    isa    => 'Str',
+    traits => [qw( SetOnce )],
+);
+
+sub BUILD {
+    my ($self) = @_;
+
+    my $xml = $self->req->get('char/CorporationSheet', corporationID => $self->corporation_id);
+
+    $self->name(     $xml->findvalue(q{//result/name[1]}));
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
