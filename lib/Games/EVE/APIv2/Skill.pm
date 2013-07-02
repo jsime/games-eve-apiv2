84ee52e34d725a3e323f23c08e01d5279d1f1d9a
Jon Sime <jonsime@gmail.com>
2013-07-02 19:32:01 -0400

    Skill class for retrieving and searching the game's skill tree -- uses a class attribute, as the data very rarely changes (i.e. months in between changes), and the document returned by the remote API is 400KB+ of XML... would still like to make the remote API call even lazier, so it's only issued on the first instance of an object needing information from the API

---
+++ lib/Games/EVE/APIv2/Skill.pm
@@ -0,0 +1,117 @@
+package Games::EVE::APIv2::Skill;
+
+=head1 NAME
+
+Games::EVE::APIv2::Skill
+
+=head1 SYNOPSIS
+
+Subclass providing access to EVE Online's skill tree information. Will perform
+a remote API call the first time any skill tree information is requested, and
+will then cache that data as a class variable.
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use MooseX::ClassAttribute;
+use namespace::autoclean;
+
+extends 'Games::EVE::APIv2::Base';
+
+# Override key_id and v_code attributes, since we don't/can't use them on SkillTree
+# API calls (you get a 403 forbidden from the CCP servers if you try to supply them).
+has 'key_id' => (
+    is        => 'rw',
+    isa       => 'Maybe[Any]',
+    traits    => [qw( SetOnce )],
+    predicate => 'has_key_id',
+);
+
+has 'v_id' => (
+    is        => 'rw',
+    isa       => 'Maybe[Any]',
+    traits    => [qw( SetOnce )],
+    predicate => 'has_v_code',
+);
+
+class_has 'Cache' => (
+    is        => 'rw',
+    isa       => 'HashRef[HashRef]',
+    predicate => 'is_cached'
+);
+
+has 'skill_id' => (
+    is        => 'rw',
+    isa       => 'Int',
+    traits    => [qw( SetOnce )],
+    predicate => 'has_skill_id',
+);
+
+has 'name' => (
+    is        => 'rw',
+    isa       => 'Str',
+    traits    => [qw( SetOnce )],
+    predicate => 'has_name',
+);
+
+has 'description' => (
+    is     => 'rw',
+    isa    => 'Str',
+    traits => [qw( SetOnce )],
+);
+
+sub update_cache {
+    my ($self) = @_;
+
+print STDERR " ** Updating SkillTree Cache...\n";
+    my $xml = $self->req->get('eve/SkillTree');
+
+print STDERR " ** Got " . length($xml->toString) . " bytes of XML\n";
+    my %groups;
+    my %skills;
+
+    foreach my $groupnode ($xml->findnodes(q{//result/rowset[@name='skillGroups']/row})) {
+        $groups{$groupnode->findvalue(q{@groupID})} = $groupnode->findvalue(q{@groupName});
+    }
+print STDERR " ** Found " . scalar(keys(%groups)) . " skill groups\n";
+
+    foreach my $skillnode ($xml->findnodes(q{//rowset[@name='skills']/row})) {
+        $skills{$skillnode->findvalue(q{@typeID})} = {
+            name        => $skillnode->findvalue(q{@typeName}),
+            description => $skillnode->findvalue(q{description[1]}),
+        }
+    }
+print STDERR " ** Found " . scalar(keys(%skills)) . " skills\n";
+
+    $self->Cache({ groups => \%groups, skills => \%skills });
+}
+
+sub BUILD {
+    my ($self) = @_;
+
+    $self->update_cache unless $self->is_cached;
+
+    my $skill_id;
+    if ($self->has_skill_id) {
+        $skill_id = $self->skill_id;
+    } elsif ($self->has_name) {
+        $skill_id = (grep { $self->Cache->{'skills'}{$_}{'name'} eq $self->name }
+                        keys %{$self->Cache->{'skills'}})[0];
+    }
+
+    die "Cannot look up skill with at least an ID or a name." unless defined $skill_id;
+    die "Invalid Skill ID: $skill_id" unless exists $self->Cache->{'skills'}{$skill_id};
+
+    my $skill = $self->Cache->{'skills'}{$skill_id};
+
+    $self->skill_id($skill_id) unless $self->has_skill_id;
+    $self->name($skill->{'name'}) unless $self->has_name;
+    $self->description($skill->{'description'});
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
