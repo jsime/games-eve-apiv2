cddb0f92424d06868f6f58d302d7730db33e1c50
Jon Sime <jonsime@gmail.com>
2013-07-06 23:34:19 -0400

    fix short-circuit logic for when to call remote API for updating cache

--- lib/Games/EVE/APIv2/Skill.pm
+++ lib/Games/EVE/APIv2/Skill.pm
@@ -131,8 +131,8 @@ sub check_cache {
 
     my $has_attr = 'has_' . $attr;
 
-    return if $self->is_cached || $self->$has_attr;
-    $self->update_cache;
+    return if $self->is_cached && $self->$has_attr;
+    $self->update_cache unless $self->is_cached;
     $self->check_called(1);
 
     my $skill;
