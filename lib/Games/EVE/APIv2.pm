c38eb2d7f327a3cd2c374e9112e5b377e7d01535
Jon Sime <jonsime@gmail.com>
2013-07-13 22:08:43 -0400

    minor version bump for internal packaging

--- lib/Games/EVE/APIv2.pm
+++ lib/Games/EVE/APIv2.pm
@@ -6,11 +6,11 @@ Games::EVE::APIv2 - Perl interface to CCP's API (version 2) for EVE Online
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
