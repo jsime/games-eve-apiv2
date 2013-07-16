1c1a73880321485b9a0b1b5d33a909c8ac33125a
Jon Sime <jonsime@gmail.com>
2013-07-16 18:27:30 -0400

    updates to Request class to handle new Key objects, including proper exclusion of key information from API calls when the key is not appropriate for the given character/corporation (and we can only access the public versions of the API data)

--- lib/Games/EVE/APIv2/Request.pm
+++ lib/Games/EVE/APIv2/Request.pm
@@ -27,21 +27,24 @@ use namespace::autoclean;
 has 'api' => (
     is        => 'rw',
     isa       => 'Str',
-    clearer   => 'clear_api',
     predicate => 'has_api',
+);
+
+has 'key' => (
+    is        => 'rw',
+    isa       => 'Games::EVE::APIv2::Key',
+    predicate => 'has_key',
 );
 
 has 'key_id' => (
     is        => 'rw',
-    isa       => 'Maybe[Int]',
-    clearer   => 'clear_key_id',
+    isa       => 'Int',
     predicate => 'has_key_id',
 );
 
 has 'v_code' => (
     is        => 'rw',
-    isa       => 'Maybe[Str]',
-    clearer   => 'clear_v_code',
+    isa       => 'Str',
     predicate => 'has_v_code',
 );
 
@@ -53,24 +56,25 @@ This library exports nothing but its constructor.
 
 =head2 get
 
-When invoked, this method makes a remote HTTP(s) call to the CCP API endpoint
+When invoked, this method makes a remote HTTPS call to the CCP API endpoint
 specified. While the parameters differ between API methods, they all require
 at the very least the path to the API method and the standard Key ID and
 Verification Code.
 
-This method will use the Key ID and Verification Code supplied during object
-construction, but you may also override these on a per-call basis by passing
-in an options hash (keys: C<key_id> and C<v_code>).
+Some API calls require additional parameters, beyond just key information
+(which is included automatically, based on the key_id/v_code pair or key
+object passed in during construction). For these API calls, the API name
+should be followed by a list of key/value pairs (a bare hash, not a hash
+reference) for those parameters.
 
-The API method to be called may be specified in either of two ways: as the
-first argument in scalar form, or as part of the options hash with the key
-name C<api>.
+As an example, calling the character sheet API requires supplying a
+Character ID. The second example below demonstrates this usage. Note that
+the key names must match the URL parameters documented for CCP's API.
 
 Examples:
 
     $xml = $eve_req->get('account/APIKeyInfo');
-    $xml = $eve_req->get('account/APIKeyInfo', key_id => '123', v_code => 'abc');
-    $xml = $eve_req->get( api => 'account/APIKeyInfo' );
+    $xml = $eve_req->get('char/CharacterSheet', characterID => 12345 );
 
 Note that the API method is named by only the path portion of the official
 CCP URL, minus the file extensions generally present.
@@ -82,46 +86,67 @@ C<die()> is called.
 
 sub get {
     my $self = shift;
+    my $api = shift;
 
-    my ($api);
-    $api = $self->api if $self->has_api;
-    $api = shift if defined $_[0] && !ref($_[0]);
+    die "No API path specified." unless defined $api && length($api) > 0;
 
     my %opts = @_;
-    $api = $opts{'api'} if exists $opts{'api'};
+
+    if (exists $opts{'key_id'}) {
+        $opts{'keyID'} = $opts{'key_id'};
+        delete $opts{'key_id'};
+    }
+    if (exists $opts{'v_code'}) {
+        $opts{'vCode'} = $opts{'v_code'};
+        delete $opts{'v_code'};
+    }
+
+    $opts{'keyID'} = $self->key->key_id if $self->has_key;
+    $opts{'vCode'} = $self->key->v_code if $self->has_key;
 
     # The following API calls cannot be made with key information (they don't
-    # require it, and will actually error out if given key info)
+    # require it, and will actually error out if given key info).
     my @no_key_apis = qw(
         eve/AllianceList
         eve/CertificateTree
         eve/SkillTree
     );
 
+    # The following API calls take a character ID, but should only be given a
+    # keyid/vcode pair if the current key is valid for that character. Otherwise
+    # the key information should be omitted.
+    my @charid_apis = qw(
+        char/CharacterSheet
+    );
+
+    # Same as above, but for corporations instead of characters.
+    my @corpid_apis = qw(
+        corp/CorporationSheet
+    );
+
     if (grep { $api eq $_ } @no_key_apis) {
-        %opts = ();
-    } else {
-        die "Cannot call APIv2 without API path, Key ID and Verification Code"
-            unless defined $api && $self->has_key_id && $self->has_v_code;
-
-        if (exists $opts{'key_id'}) {
-            $opts{'keyID'} = $opts{'key_id'};
-            delete $opts{'key_id'};
-        }
-        if (exists $opts{'v_code'}) {
-            $opts{'vCode'} = $opts{'v_code'};
-            delete $opts{'v_code'};
+        delete $opts{'keyID'} if exists $opts{'keyID'};
+        delete $opts{'vCode'} if exists $opts{'vCode'};
+    } elsif ($self->has_key) {
+        if (exists $opts{'characterID'} && grep { $api eq $_ } @charid_apis) {
+            unless ($self->key->for_character($opts{'characterID'})) {
+                delete $opts{'keyID'} if exists $opts{'keyID'};
+                delete $opts{'vCode'} if exists $opts{'vCode'};
+            }
         }
 
-        $opts{'keyID'} = $self->key_id if !exists $opts{'keyID'} && $self->has_key_id;
-        $opts{'vCode'} = $self->v_code if !exists $opts{'vCode'} && $self->has_v_code;
-        delete $opts{'api'} if exists $opts{'api'};
+        if (exists $opts{'corporationID'} && grep { $api eq $_ } @corpid_apis) {
+            unless ($self->key->for_corporation($opts{'corporationID'})) {
+                delete $opts{'keyID'} if exists $opts{'keyID'};
+                delete $opts{'vCode'} if exists $opts{'vCode'};
+            }
+        }
     }
 
-    die "Requested API method doesn't appear valid: $api" unless $api =~ m{^[a-z]+/[a-zA-Z]+$}o;
-
+print STDERR "**** $api ****\n";
     my $api_url = 'https://api.eveonline.com/' . $api . '.xml.aspx?' .
         (join('&', map { uri_escape($_) . '=' . uri_escape($opts{$_}) } sort keys %opts)) || '';
+print STDERR "**** $api_url ****\n";
 
     my $ua = LWP::UserAgent->new();
     $ua->agent(sprintf('%s (%s %s)', $ua->_agent, 'Games::EVE::APIv2', $Games::EVE::APIv2::VERSION));
