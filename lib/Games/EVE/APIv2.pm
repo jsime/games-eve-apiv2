a25770210c6c30087ef2d213e5efb0e0d682ebcf
Jon Sime <jonsime@gmail.com>
2013-07-28 19:16:23 -0400

    version bump and Changes for the attribute big fix in 0.06

--- lib/Games/EVE/APIv2.pm
+++ lib/Games/EVE/APIv2.pm
@@ -6,11 +6,11 @@ Games::EVE::APIv2 - Perl interface to CCP's API (version 2) for EVE Online
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
