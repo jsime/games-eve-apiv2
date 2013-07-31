68ef09b96bbc4a37e48c874c6f23097a706d333a
Jon Sime <jonsime@gmail.com>
2013-07-30 23:33:40 -0400

    version bump and Changes to reflects skill_queue-created Skill object changes

--- lib/Games/EVE/APIv2.pm
+++ lib/Games/EVE/APIv2.pm
@@ -6,11 +6,11 @@ Games::EVE::APIv2 - Perl interface to CCP's API (version 2) for EVE Online
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
